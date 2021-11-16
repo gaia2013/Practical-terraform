7288402f83b8f4dfb01cf7fd3493d9f1dd6710b3
gaia_galaxy <hitoshi.gaia.galaxy@gmail.com>
2021-11-16 21:17:19 +0900

    3.3 output

--- example/main.tf
+++ example/main.tf
@@ -1,8 +1,8 @@
-locals {
-  example_instance_type = "t3.micro"
-}
-
 resource "aws_instance" "example" {
   ami	  = "ami-0c3fd0f5d33134a76"
-  instance_type = var.example_instance_type
+  instance_type = "t3.micro" 
+}
+
+output "example_instance_id" {
+  value = aws_instance.example.id
 }
