389598497228e73990726dd711c5706f6e8daedd
gaia_galaxy <hitoshi.gaia.galaxy@gmail.com>
2021-11-16 22:55:04 +0900

    reference

--- example/main.tf
+++ example/main.tf
@@ -1,3 +1,21 @@
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port =	0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-1"
 }
@@ -19,6 +37,16 @@ data "aws_ami" "recent_amazon_linux_2" {
 
 resource "aws_instance" "example" {
   ami	  = data.aws_ami.recent_amazon_linux_2.image_id
-  instance_type = "t3.micro" 
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.example_ec2.id]
+
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+EOF
 }
 
+output "example_public_dns" {
+  value = aws_instance.example.public_dns
+}
