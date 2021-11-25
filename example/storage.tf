3998679c2137eeb6f7d1cf4f6f8c52ed3318449e
gaia_galaxy <hitoshi.gaia.galaxy@gmail.com>
2021-11-25 21:36:34 +0900

    block public access

--- example/storage.tf
+++ example/storage.tf
@@ -13,3 +13,12 @@ resource "aws_s3_bucket" "private" {
     }
   }
 }
+
+# 特に理由がなければ、すべての設定を有効にする。
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket	    = aws_s3_bucket.private.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
