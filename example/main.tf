9bf5206a14ece6e411a616034d09420e8226aae4
gaia_galaxy <hitoshi.gaia.galaxy@gmail.com>
2021-11-25 19:48:54 +0900

    chapter5_authority_management

--- example/main.tf
+++ example/main.tf
@@ -1,8 +1,17 @@
-module "web_server" {
-  source	= "./http_server"
-  instance_type = "t3.micro"
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow", # Allow or Deny
+      "Action": ["ec2:DescribeRegions"], # リージョン一覧を取得する。なんのサービスで、どんな操作が実行できるか
+      "Resource": ["*"] # 操作可能なリソースはなにか
+    }
+  ]
 }
 
-output "public_dns" {
-  value = module.web_server.public_dns
+module "describe_regions_for_ec2" {
+  source = "./iam_role"
+  name	= "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy  = data.aws_iam_policy_document.allow_describe_regions.json
 }
